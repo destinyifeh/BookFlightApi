775b4e884feaa83500321d63c86f3538d7302e6e
destiny.ifehor <destiny.ifehor@interswitchroup.com>
2022-10-18 10:47:44 +0100

    updated

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,81 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
-}
+const { Flights } = require("../models/Flight");
+const { v4: uuid } = require("uuid");
 
+exports.addFlight = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { title, price } = await req.body;
+    const newFlight = {
+      id: uuid(),
+      title,
+      price,
+      time: new Date().toLocaleDateString(),
+      date: new Date().toLocaleTimeString(),
+    };
+    Flights.push(newFlight);
+    res.status(200).json({ message: "Flight created", newFlight: newFlight });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
 
+exports.getFlights = async (req, res) => {
+  try {
+    const flights = Flights;
+    res.status(200).json({ message: "All flights", flights });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getFlight = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let flight = Flights.find((flight) => flight.id === id);
+    if (flight) {
+      res.status(200).json({ message: "Flight found", flight: flight });
+    } else {
+      res.status(404).json({ message: "Flight not found" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateFlight = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let flight = Flights.find((flight) => flight.id === id);
+    const { title, price } = await req.body;
+    if (flight) {
+      (flight.title = title),
+        (flight.price = price),
+        res.status(200).json({ message: "Flight found", flight: flight });
+    } else {
+      res.status(404).json({ message: "Flight not found" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deleteFlight = async (req, res) => {
+  try {
+    let id = req.params.id;
+
+    let flight = Flights.find((flight) => flight.id === id);
+    if (flight) {
+      Flights.splice(Flights.indexOf(flight), 1);
+      res.status(200).json({ message: "delated", flight });
+    } else {
+      res.status(404).json({ message: "Flight not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
--- index.js
+++ index.js
@@ -1,109 +1,13 @@
 const express = require("express");
 const { json } = require("express");
-const flights = require("./controllers/flightController");
-const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
-const Book = models;
 const app = express();
 
 app.use(json());
 
-app.use("/", routes);
+app.use("/flight", routes);
 
-//Add/Book Flight
-
-app.post("/books", function (req, res) {
-  Book.create(
-    {
-      title: "flight to canada",
-      time: "1pm",
-      price: 26000,
-      date: "26-06-2022",
-    },
-    (err, newBook) => {
-      if (err) {
-        return res.status(500).json("Server error:" + "" + err);
-      } else {
-        return res
-          .status(200)
-          .json({ message: "New book created successfully", newBook });
-      }
-    }
-  );
-});
-
-//Get all flight
-
-app.get("/books", (req, res) => {
-  Book.find({}, (err, books) => {
-    if (err) {
-      return res.status(500).json({ message: err });
-    } else {
-      return res.status(200).json(books);
-    }
-  });
-});
-
-//Get single flight
-
-app.get("/books/:id", (req, res) => {
-  Book.findOne({ _id: req.params.id }, (err, book) => {
-    if (err) {
-      return res.status(500).json({ message: err });
-    } else if (!book) {
-      return res.status(404).json({ message: "Book not found" });
-    } else {
-      return res.status(200).json(book);
-    }
-  });
-});
-
-//Update/Edit Flight
-
-app.put("/book/:id", (req, res) => {
-  Book.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      title: "flight to canada",
-      time: "1pm",
-      price: 26000,
-      date: "26-06-2022",
-    },
-    (err, book) => {
-      if (err) {
-        return res.status(500).json({ message: err });
-      } else if (!book) {
-        return res.status(404).json({ message: "Book not found" });
-      } else {
-        book.save((err, saveBook) => {
-          if (err) {
-            return res.status(500).json({ message: err });
-          } else {
-            return res
-              .status(200)
-              .json({ message: "Book updated successfully", saveBook });
-          }
-        });
-      }
-    }
-  );
-});
-
-//Delete flight
-
-app.delete("/books/:id", (req, res) => {
-  Book.findByIdAndDelete(req.params.id, (err, book) => {
-    if (err) {
-      return res.status(500).json({ message: err });
-    } else if (!book) {
-      return res.status(404).json({ message: "Book not found" });
-    } else {
-      return res.status(200).json({ message: "Book deleted successfully" });
-    }
-  });
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
